95bd610a09c218e810d7bd5699614f39d567b3bd
Bohdan Karpenchuk <bodixc@gmail.com>
2021-09-20 13:39:28 +0300

    first modification

---
+++ Lab1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float dx;
+    cout << "Введіть крок інтегрування: ";
+    cin >> dx;
+    float a = 1;
+    float int_func = 0;
+    for (float x = -1; x <= a; x += dx)
+    {
+        int_func += abs(dx * cos(x));
+        cout << int_func << " " << "if x=" << x << endl;
+
+    }
+    return 0;
+}
+
+
+
+
+
+
