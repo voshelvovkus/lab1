c7722d81e8c2c29c9c84a160fedba3054c37726a
Bohdan Karpenchuk <bodixc@gmail.com>
2021-09-20 14:05:53 +0300

    Ver 1.0

--- Lab1.cpp
+++ Lab1.cpp
@@ -14,7 +14,7 @@ int main()
     for (float x = -1; x <= a; x += dx)
     {
         int_func += abs(dx * cos(x));
-        cout << int_func << " " << "if x=" << x << endl;
+        cout << int_func << " " << "if x=" << x << endl; //виводить значення інтегралу залежно від кроку інтегрування та значення х
 
     }
     return 0;
